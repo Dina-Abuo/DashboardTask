af7d08c11aadc246f9b5cf370cfcf710962b182d
Dina-Abuo <dynaalkhfajy73@gmail.com>
2024-06-06 21:56:24 +0300

    implement  reight side component

--- src/app/components/dashboard/rightside/rightside.component.ts
+++ src/app/components/dashboard/rightside/rightside.component.ts
@@ -6,17 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./rightside.component.css'],
 })
 export class RightSideComponent {
-  selectedRam: number = 90;
-  value: number = 90;
-  min: number = 90;
-  max: number = 90;
-
-  setRam(value: number) {
-    this.selectedRam = value;
-    console.log(this.selectedRam);
-  }
-  // document.getElementById("myinput").oninput = function() {
-  //   var value = (this.value-this.min)/(this.max-this.min)*100
-  //   this.style.background = 'linear-gradient(to right, #82CFD0 0%, #82CFD0 ' + value + '%, #fff ' + value + '%, white 100%)'
-  // };
 }
